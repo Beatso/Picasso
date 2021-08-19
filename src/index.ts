f0dcc2baa02954c5c62e5895066c9943f0204f11
Beatso <39217836+Beatso@users.noreply.github.com>
2021-08-19 21:50:05 +0100

    basic client set up

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("hello world")
+import { Client, Intents } from "discord.js"
+import { config as dotenvConfig } from "dotenv"
+
+dotenvConfig()
+
+const client = new Client({
+	intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES],
+})
+
+// client logs in
+client.once("ready", () => {
+	console.log("Logged in as", client.user?.tag)
+
+	// set status
+	;(function updateStatus() {
+		client.user?.setActivity(`${client.guilds.cache.size} servers`, {
+			type: "WATCHING",
+		})
+
+		// update every four hours
+		setTimeout(updateStatus, 60 * 60 * 1000 * 4)
+	})()
+})
+
+// log in client
+client.login(process.env.bottoken)
